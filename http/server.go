3a43936ef80a600a462c59d70100c0a64af0e961
Marek Cermak <marek.cermak@strv.com>
2022-06-23 11:51:55 +0200

    feat: add SIGINT and SIGTERM signal handler
    Signed-off-by: Marek Cermak <marek.cermak@strv.com>

--- http/server.go
+++ http/server.go
@@ -2,10 +2,12 @@ package http
 
 import (
 	"context"
-	"fmt"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
 
@@ -22,6 +24,8 @@ func NewServer(config *ServerConfig) *Server {
 			Handler:        config.Handler,
 			MaxHeaderBytes: config.Limits.MaxHeaderBytes,
 		},
+		signalsListener:  make(chan os.Signal, 1),
+		waitForShutdown:  make(chan struct{}, 1),
 		doBeforeShutdown: config.Hooks.BeforeShutdown,
 	}
 	if to := config.Limits.Timeouts; to != nil {
@@ -33,17 +37,14 @@ func NewServer(config *ServerConfig) *Server {
 		s.shutdownTimeout = to.ShutdownTimeout
 	}
 
-	s.server.RegisterOnShutdown(func() {
-		s.inShutdown <- struct{}{}
-	})
-
+	s.server.RegisterOnShutdown(s.beforeShutdown)
 	return s
 }
 
 type Server struct {
 	server http.Server
 
-	inShutdown      chan struct{}
+	signalsListener chan os.Signal
 	shutdownTimeout time.Duration
 	waitForShutdown chan struct{}
 
@@ -57,42 +58,44 @@ func (s *Server) Start(ctx context.Context) error {
 		return ctx
 	}
 
+	signal.Notify(s.signalsListener, syscall.SIGINT, syscall.SIGTERM)
+
+	errCh := make(chan error, 1)
 	go func() {
-		<-s.inShutdown
-		s.waitForShutdown = make(chan struct{})
-		// NOTE: The context will be canceled when the server is shutdown.
-		s.beforeShutdown(ctx)
-		s.waitForShutdown <- struct{}{}
+		errCh <- s.server.ListenAndServe()
 	}()
-	if err := s.server.ListenAndServe(); err != http.ErrServerClosed {
-		return err
+
+	select {
+	case <-errCh:
+		// TODO: Log error.
+	case <-ctx.Done():
+		// TODO: Log context closed.
+	case <-s.signalsListener:
+		// TODO: Log signal received.
 	}
-	if s.waitForShutdown != nil {
-		if s.shutdownTimeout > 0 {
-			select {
-			case <-s.waitForShutdown:
-				return nil
-			case <-time.After(s.shutdownTimeout):
-				return fmt.Errorf("shutdown timeout %s exceeded", s.shutdownTimeout)
-			}
-		}
-		<-s.waitForShutdown
+
+	// TODO: Log server shutdown error.
+	_ = s.server.Shutdown(context.Background())
+	select {
+	case <-s.waitForShutdown:
+		return nil
+	case <-time.After(s.shutdownTimeout):
+		return ErrShutdownTimeout
 	}
-	return nil
 }
 
-func (s *Server) beforeShutdown(ctx context.Context) {
+func (s *Server) beforeShutdown() {
 	wg := &sync.WaitGroup{}
 	wg.Add(len(s.doBeforeShutdown))
 
 	for _, f := range s.doBeforeShutdown {
 		go func(f ServerHookFunc, wg *sync.WaitGroup) {
-			// TODO: We should probably handle this error somehow, or at least log warning that the hook failed.
-			_ = f(ctx)
+			f(context.Background())
 			wg.Done()
 		}(f, wg)
 	}
 	wg.Wait()
+	s.waitForShutdown <- struct{}{}
 }
 
 // ServerConfig represents Server configuration.
@@ -145,4 +148,4 @@ type ServerHooks struct {
 	BeforeShutdown []ServerHookFunc
 }
 
-type ServerHookFunc func(context.Context) error
+type ServerHookFunc func(context.Context)
