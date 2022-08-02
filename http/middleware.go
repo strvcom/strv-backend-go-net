32a91d2de989e88dee91d8cef94ca125d8c88eba
Tomas Kocman <tomas.kocman@strv.com>
2022-08-02 07:43:43 +0200

    docs: add docstrings for middlewares

--- http/middleware.go
+++ http/middleware.go
@@ -13,9 +13,13 @@ const (
 	requestIDLogFieldName = "request_id"
 )
 
+// RequestIDFunc is used for obtaining a request ID from the HTTP header.
 type RequestIDFunc func(h http.Header) string
 
-func RequestIDMiddleware(f RequestIDFunc) func(handler http.Handler) http.Handler {
+// RequestIDMiddleware saves request ID into the request context.
+// If context already contains request ID, next handler is called.
+// If the user provided function returns empty request ID, a new one is generated.
+func RequestIDMiddleware(f RequestIDFunc) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if requestID := net.RequestIDFromCtx(r.Context()); requestID != "" {
@@ -36,7 +40,10 @@ func RequestIDMiddleware(f RequestIDFunc) func(handler http.Handler) http.Handle
 	}
 }
 
-func RecoverMiddleware(l logger.ServerLogger) func(handler http.Handler) http.Handler {
+// RecoverMiddleware calls next handler and recovers from a panic.
+// If a panic occurs, log this event, set http.StatusInternalServerError as a status code
+// and save a panic object into the response writer.
+func RecoverMiddleware(l logger.ServerLogger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			defer func() {
@@ -49,7 +56,6 @@ func RecoverMiddleware(l logger.ServerLogger) func(handler http.Handler) http.Ha
 					rw.SetPanicObject(re)
 					rw.WriteHeader(http.StatusInternalServerError)
 
-					net.RequestIDFromCtx(r.Context())
 					l.With(
 						logger.Any("err", re),
 						logger.Any(requestIDLogFieldName, net.RequestIDFromCtx(r.Context())),
@@ -61,7 +67,15 @@ func RecoverMiddleware(l logger.ServerLogger) func(handler http.Handler) http.Ha
 	}
 }
 
-func LoggingMiddleware(l logger.ServerLogger) func(handler http.Handler) http.Handler {
+// LoggingMiddleware logs:
+//   - URL path
+//   - HTTP method
+//   - Request ID
+//   - Duration of a request
+//   - HTTP status code
+//   - Panic object if exists
+// If the status code >= http.StatusInternalServerError, logs with error level, info otherwise.
+func LoggingMiddleware(l logger.ServerLogger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			rw, ok := w.(*internal.ResponseWriter)
@@ -92,11 +106,11 @@ func LoggingMiddleware(l logger.ServerLogger) func(handler http.Handler) http.Ha
 	}
 }
 
-// LogData contains processed request data for purposes of logging.
+// LogData contains processed request data for logging purposes.
 // Path is path from URL of the request.
-// Method is http request method.
+// Method is HTTP request method.
 // Duration is how long it took to process whole request.
-// ResponseStatusCode is http status code which was returned.
+// ResponseStatusCode is HTTP status code which was returned.
 // RequestID is unique identifier of request.
 // Panic is panic object containing error message.
 type LogData struct {
@@ -108,7 +122,7 @@ type LogData struct {
 	Panic              any
 }
 
-// WithData returns logger with filled fields based on provided logging settings.
+// WithData returns logger with filled fields.
 func WithData(l logger.ServerLogger, ld LogData) logger.ServerLogger {
 	l = l.With(
 		logger.Any("method", ld.Method),
